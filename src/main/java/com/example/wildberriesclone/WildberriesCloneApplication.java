0c73a1a5eedb6fd4539313fe2a9bef21a62afe9b
Katsiaryna Lasitsa <katya.lasicza.03@mail.ru>
2023-09-13 21:51:17 +0300

    Initial commit

---
+++ src/main/java/com/example/wildberriesclone/WildberriesCloneApplication.java
@@ -0,0 +1,13 @@
+package com.example.wildberriesclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WildberriesCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WildberriesCloneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wildberriesclone/WildberriesCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wildberriesclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WildberriesCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
