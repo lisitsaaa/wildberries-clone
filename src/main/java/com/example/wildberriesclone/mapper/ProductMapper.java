ff6e3d79243e481b55a25b10fdee3a379b27bdfd
Katsiaryna Lasitsa <katya.lasicza.03@mail.ru>
2023-10-20 13:06:09 +0300

    Added mapper classes - ProductMapper and ProductPropertyMapper

---
+++ src/main/java/com/example/wildberriesclone/mapper/ProductMapper.java
@@ -0,0 +1,14 @@
+package com.example.wildberriesclone.mapper;
+
+import com.example.wildberriesclone.dto.product.ProductDto;
+import com.example.wildberriesclone.entity.product.Product;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface ProductMapper {
+    ProductMapper INSTANCE = Mappers.getMapper(ProductMapper.class);
+
+    Product dtoToProduct(ProductDto productDto);
+    ProductDto productToDto(Product product);
+}
