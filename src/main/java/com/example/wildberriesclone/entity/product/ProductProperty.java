d12be655a1c9cadeb64532d99f0136c062d8c388
Katsiaryna Lasitsa <katya.lasicza.03@mail.ru>
2023-10-20 12:58:24 +0300

    Removed a field product

--- src/main/java/com/example/wildberriesclone/entity/product/ProductProperty.java
+++ src/main/java/com/example/wildberriesclone/entity/product/ProductProperty.java
@@ -1,11 +1,9 @@
 package com.example.wildberriesclone.entity.product;
 
 import com.example.wildberriesclone.entity.AbstractEntity;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 
 import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
 
 @Entity
 @Setter @Getter
@@ -13,8 +11,4 @@ import javax.persistence.ManyToOne;
 public class ProductProperty extends AbstractEntity {
     private String name;
     private String description;
-
-    @ManyToOne
-    @JsonIgnore
-    private Product product;
 }
