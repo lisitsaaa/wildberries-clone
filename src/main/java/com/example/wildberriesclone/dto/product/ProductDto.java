7ec7098bc4f061e9d63c488bf1e92a49ca032643
Katsiaryna Lasitsa <katya.lasicza.03@mail.ru>
2023-10-20 12:59:42 +0300

    Added dto classes for product

---
+++ src/main/java/com/example/wildberriesclone/dto/product/ProductDto.java
@@ -0,0 +1,22 @@
+package com.example.wildberriesclone.dto.product;
+
+import com.example.wildberriesclone.dto.productProperty.ProductPropertyListDto;
+import com.example.wildberriesclone.dto.user.UserInfoDto;
+import com.example.wildberriesclone.entity.product.Category;
+import com.example.wildberriesclone.entity.product.ProductStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor @AllArgsConstructor
+public class ProductDto {
+    private String name;
+    private double price;
+    private String article;
+    private String description;
+    private UserInfoDto seller;
+    private Category category;
+    private ProductStatus status;
+    private ProductPropertyListDto productProperties;
+}
